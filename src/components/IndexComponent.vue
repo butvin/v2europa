fa2ca76b4799833d9558d4f4aae07987d57bb811
butvin-home-sumsung <butvin-dev@gmail.com>
2020-11-09 09:14:07 +0200

    start development

---
+++ src/components/IndexComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index-component">
+    <h1>{{ componentName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexComponent',
+  props: {
+    componentName: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
