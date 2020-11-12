b814dc48e6c70d73728a1fd6930867ea2cd4c45f
butvin-home-sumsung <butvin-dev@gmail.com>
2020-11-12 07:15:00 +0200

    register-component: configured & attached validator

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="example-component">
+    <b-container>
+      <b-row>
+        <b-col>
+          <p class="display-1">Example Component</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleComponent',
+  data () {
+    // toDo:
+  }
+}
+</script>
+
+<style scoped>
+.example-component {
+  border: 4px solid red;
+  background: #272625;
+  color: #f9d423;
+}
+</style>
---
+++ src/components/auth/elements/InputField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="input-field-component">
+    <b-container>
+      <b-row>
+        <b-col>
+          <p class="display-4">Login Component</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputField'
+}
+</script>
+
+<style scoped>
+.input-field-component {
+}
+</style>
